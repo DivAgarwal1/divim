9ccd705b7903aa5a6d4810d7e1753000ecf046c3
DivAgarwal1 <divca06@gmail.com>
2025-12-24 17:47:54 -0800

    Fix paths

--- lua/divim/init.lua
+++ lua/divim/init.lua
@@ -1,4 +1,4 @@
-require("options")
-require("keymaps")
-require("lazy_init")
-require("autocmds")
+require("divim.options")
+require("divim.keymaps")
+require("divim.lazy_init")
+require("divim.autocmds")
